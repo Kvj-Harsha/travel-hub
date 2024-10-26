b9b0de383864cdca33a8b3576c9b14c646139afe
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-10-26 18:40:53 +0530

    change to check

--- app/About/page.js
+++ app/About/page.js
@@ -4,7 +4,7 @@ import Errorpage from '../_components/Errorpage'
 function About() {
   return (
     <div>
-        <h1 align="center">Welcome to about</h1>
+        <h1 align="center">Welcome to About</h1>
       <Errorpage/>
     </div>
   )
