71c2b0975814136dde010b5f4227d9ed58e1ed89
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-10-26 21:33:04 +0530

    new chnages

--- app/About/page.js
+++ app/About/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Errorpage from '../_components/Errorpage'
+import Headerdarknext from '../_components/Headerdarknxt'
 
 function About() {
   return (
     <div>
+        <Headerdarknext/>
         <h1 align="center">Welcome to About</h1>
       <Errorpage/>
     </div>
