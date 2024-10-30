9e6c6d5621bd8512e491c4413a0a61995350e089
Y-Santhosh <ad23b1060@iiitr.ac.in>
2024-10-30 19:34:27 +0530

    Done small changes

--- app/Create-trip/page.js
+++ app/Create-trip/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Headerdarknext2 from '../_components/Headerdarknxt2'
+// import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 function CreateTrip() {
   return (
@@ -10,9 +11,16 @@ function CreateTrip() {
             <h2 className='font-bold textsixe:3xl'>Tell us your travel preferences</h2>
             <p mt-3 className='mt-3 text-grey-500 text-xl'>Just provide some basic information and our trip planner 
               will generate a itenarary based on your preference</p>
-          <div className='mt-20'>
+          <div className='mt-20 flex flex-col gap-10'>
             <div>
               <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+              {/* <GooglePlacesAutocomplete
+                apiKey=''
+              /> */}
+              <div>
+                <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip?</h2>
+                <input placeholder={'Ex.3'} type='number' />  
+              </div> 
             </div>
           </div>
       </div>
