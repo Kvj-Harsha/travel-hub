48677ec2d8a230bae9010b48ccee4d83ba7bd3c6
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-11-02 14:34:05 +0530

    small chnages

--- app/Tripplanner/Create-trip/page.js
+++ app/Tripplanner/Create-trip/page.js
@@ -1,5 +1,6 @@
 import Headerdarknext from '@/app/_components/Headerdarknxt'
 import Itenary from '@/app/_components/Itenary'
+import TripCard from '@/app/_components/TripCard'
 import React from 'react'
 
 function page() {
@@ -7,6 +8,7 @@ function page() {
     <div>
       <Headerdarknext/>
       <Itenary/>
+
     </div>
   )
 }
