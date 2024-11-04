f5ecf3660b5e581d970357adbed57fc2829fae02
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-11-04 16:45:10 +0530

    vercel analytics integration

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Footer from "./_components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           {/* <UserButton showName/> */}
         </SignedIn>
         {children}
+        <Analytics />
         <Footer/>
         </body>
     </html>
